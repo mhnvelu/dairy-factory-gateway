7d787cf43b1b09a8e93736d1b2eb0a7bd788c94f
Mohanavelu Subramanian <mhnvelu@gmail.com>
2021-06-02 11:24:16 +0100

    Initial Project Setup

---
+++ src/main/java/com/spring/microservices/dairyfactorygateway/DairyFactoryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.dairyfactorygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairyFactoryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DairyFactoryGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/microservices/dairyfactorygateway/DairyFactoryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.dairyfactorygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyFactoryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
