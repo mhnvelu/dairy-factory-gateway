42513fd4313228820415f7e1fb93f7d6e26d58c9
Mohanavelu Subramanian <mhnvelu@gmail.com>
2021-06-02 14:10:22 +0100

    Added Routes for dairy-factory-service, dairy-factory-order-service

---
+++ src/main/java/com/spring/microservices/dairyfactorygateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,17 @@
+package com.spring.microservices.dairyfactorygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostRouteConfig {
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(r -> r.path("/api/v2/butter*", "/api/v2/butter/*", "/api/v2/butterUpc/*").uri("http://localhost:8080"))
+                .route(r -> r.path("/api/v1/customers/**").uri("http://localhost:8081"))
+                .build();
+    }
+}
